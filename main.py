892c6cc6ad699aee1f049171c848bbde34370499
Ghoster04 <diquijoaquimnamueto@gmail.com>
2025-09-22 10:14:51 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1231,5 +1231,5 @@ def initialize_database():
 import uvicorn
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # usa a PORT do Railway
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port, reload=False)
+    port = int(os.environ.get("PORT", 8000))  # pega a PORT do Railway
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
